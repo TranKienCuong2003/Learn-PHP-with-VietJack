dcd463aa1fd7eca9184b1a0806128c63a287006a
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 20:24:41 +0700

    Add php_info.php file

---
+++ 1.Basic_PHP_Exercises/php_info.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/5968/5968332.png" type="image/x-icon">
+    <title>Lấy thông tin cấu hình và phiên bản của PHP</title>
+</head>
+<body>
+    <?php
+        phpinfo();
+    ?>
+</body>
+</html>
