7cb624d38d1889caec9bf114a7252da538fe1dd4
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 21:00:23 +0700

    Add day_of_week.php file

---
+++ 6.DateandTimeExercises/day_of_week.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ngày trong tuần</title>
+</head>
+<body>
+    <?php
+        echo date('l') . " the " . date('jS');
+    ?>
+</body>
+</html>
