b71860a7b006c74e667ca5e05a688729de7fb44c
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 20:25:33 +0700

    Add print_example.php file

---
+++ 1.Basic_PHP_Exercises/print_example.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>In chuỗi trong PHP</title>
+</head>
+<body>
+    <?php
+        echo "Học PHP cơ bản và nâng cao tại VietJack. <br>";   
+        echo "Ví dụ minh họa lệnh echo. Bạn cũng có thể sử dụng lệnh print."; 
+    ?>
+</body>
+</html>
