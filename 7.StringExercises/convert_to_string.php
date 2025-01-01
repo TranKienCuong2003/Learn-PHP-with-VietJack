638dc0bf231bfc27a9ee309b55f67871b28fad7d
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 21:06:25 +0700

    Add convert_to_string.php file

---
+++ 7.StringExercises/convert_to_string.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chuyển đổi thành chuỗi</title>
+</head>
+<body>
+    <?php
+        $value = 12345;
+        $stringValue = strval($value);
+        echo "Giá trị ban đầu: $value <br>";
+        echo "Giá trị sau khi chuyển thành chuỗi: $stringValue";
+    ?>
+</body>
+</html>
