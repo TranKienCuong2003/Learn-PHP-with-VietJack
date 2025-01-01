0ba44aa271c4166caf613a6fcb8ca47f634823a4
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 20:26:08 +0700

    Add redirect.php file

---
+++ 1.Basic_PHP_Exercises/redirect.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chuyển hướng</title>
+</head>
+<body>
+    <?php
+    header("Location: https://vietjack.com");
+    exit;
+    ?>
+</body>
+</html>
