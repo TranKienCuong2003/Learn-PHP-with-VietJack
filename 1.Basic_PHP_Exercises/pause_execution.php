8c1baf67be6d276863f6a6c3a08963394ec32728
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 20:24:10 +0700

    Add pause_execution.php file

---
+++ 1.Basic_PHP_Exercises/pause_execution.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dừng Thực Thi Chương Trình</title>
+</head>
+<body>
+    <h2>Dừng Thực Thi Chương Trình</h2>
+    <?php
+		  echo date('h:i:s') . "<br>";  
+		  sleep(5);  
+		  echo date('h:i:s')."<br>";
+       ?>
+</body>
+</html>
