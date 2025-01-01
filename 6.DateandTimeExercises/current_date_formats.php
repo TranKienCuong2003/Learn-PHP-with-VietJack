8c40809341a928dae6b914752fe9a3808232ebf7
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 20:58:07 +0700

    Add current_date_formats.php

---
+++ 6.DateandTimeExercises/current_date_formats.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Định dạng ngày hiện tại</title>
+</head>
+<body>
+    <?php
+        echo date('Y/m/d') . "<br>";
+        echo date('y.m.d') . "<br>";
+        echo date('d-m-y') . "<br>";
+    ?>
+</body>
+</html>
