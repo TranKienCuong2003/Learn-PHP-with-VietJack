88d9eec6c0020890be5b3f9dd26d97c67bfa6ea4
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-01 20:23:18 +0700

    Add os_info.php file

---
+++ 1.Basic_PHP_Exercises/os_info.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thông Tin Hệ Điều Hành</title>
+</head>
+<body>
+    <h2>Thông Tin Hệ Điều Hành</h2>
+    <?php
+    echo "<p>Hệ điều hành: " . PHP_OS . "</p>";
+    ?>
+</body>
+</html>
